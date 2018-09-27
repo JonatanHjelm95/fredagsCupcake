5ea36712c6ff6b7b708a88512e4a7d957045044e
Porselicious <porseg69@gmail.com>
2018-09-27 10:59:38 +0200

    dummy data scripts

---
+++ dummyData.sql
@@ -0,0 +1,2 @@
+insert into CupcakeShop.user
+values('hans123', 'hotdog45', 'mad@had.dk', 50), ('davsdu', 'nejtakdu', '123@hejsa.dk', 100);
---
+++ topping+bottomPricing.sql
@@ -0,0 +1,19 @@
+use `CupcakeShop`;
+
+insert into `CupcakeShop`.`bottom`(bottomName, price)
+values ('Chocolate', 5),
+('Vanilla', 5),
+('Nutmeg', 5),
+('Pistacio', 6),
+('Almond', 7);
+
+insert into `CupcakeShop`.`topping`(toppingName, price)
+values ('Chocolate', 5),
+('Blueberry', 5),
+('Raspberry', 5),
+('Crispy', 6),
+('Strawberry', 6),
+('Rum/Raisin', 7),
+('Orange', 8),
+('Lemon', 8),
+('Blue Cheese', 9);
