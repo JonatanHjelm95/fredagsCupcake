f280f466a0c8fe6f6198d4e6d5c8f3b841b0fb9c
mf226 <cph-mf226@cphbusiness.dk>
2018-09-27 11:36:47 +0200

    dao getuser

--- CupcakeShop/src/main/java/data/CupcakeShopDAO.java
+++ CupcakeShop/src/main/java/data/CupcakeShopDAO.java
@@ -5,9 +5,10 @@ package data;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,6 +17,7 @@ import java.util.logging.Logger;
  * @author jonab
  */
 public class CupcakeShopDAO {
+
     /*
     Methods:
     
@@ -27,42 +29,50 @@ public class CupcakeShopDAO {
     addNewUser(User user)
 //    addCupcake(Cupcake cupcake)
 //    addBottom(Bottom bottom)
-//    addTop(Top top)
+//    addTopping(Topping topping)
     
-    */
-    private final String GET_USER_BY_USERNAME = "SELECT username, password, balance FROM recipesDB.User WHERE username = ?";
+     */
+    private final String GET_USER_BY_USERNAME = "SELECT username, password, balance FROM CupcakeShop.user WHERE username = ?";
 
     private DBConnector db;
-    private Connection con;
     
-    public CupcakeShopDAO() throws Exception {        
+    public CupcakeShopDAO() {        
         try {
             this.db = new DBConnector();
-            con = db.getConnection();
         } catch (Exception ex) {
             Logger.getLogger(CupcakeShopDAO.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Couldn't connect to DB");
         }
     }
-    
-    /** Returns a User object if a matching username is found.
-     *  Returns null if username gives no match.
-     * @param username
-     * @return 
-     */
-    public User getUser(String username) {
-        if (username == null || username.isEmpty()) {
-            return null;
+/**
+ * Returns a user with the name or throws exception.
+ * @param username
+ * @return User
+ * @throws Exception 
+ */
+    public User getUser(String username) throws Exception {
+        User user = null;
+        try {
+            Connection con = db.getConnection();
+            PreparedStatement pStatement = con.prepareStatement(GET_USER_BY_USERNAME);
+            pStatement.setString(1, username);
+            ResultSet rs = pStatement.executeQuery();
+            
+            if(rs.next()) {
+                String name = rs.getString("username");
+                String password = rs.getString("password");
+                int balance = rs.getInt("balance");
+                user = new User(username, password, balance);
+            } else {
+                throw new Exception("That user doesn't exist");
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
         }
-        
-        
-        
-        return null;
+        return user;
     }
-    
 
     public void addNewUser(User user) {
-        
-        
+
     }
 }
