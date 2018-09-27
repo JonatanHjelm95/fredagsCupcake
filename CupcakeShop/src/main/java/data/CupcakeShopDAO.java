ac2e1642cae8ebc7641325485b86e8c7a6e21bfb
mf226 <cph-mf226@cphbusiness.dk>
2018-09-27 11:06:28 +0200

    dao stuff

--- CupcakeShop/src/main/java/data/CupcakeShopDAO.java
+++ CupcakeShop/src/main/java/data/CupcakeShopDAO.java
@@ -1,9 +1,11 @@
+package data;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package data;
+
 
 import java.sql.Connection;
 import java.util.logging.Level;
@@ -14,20 +16,53 @@ import java.util.logging.Logger;
  * @author jonab
  */
 public class CupcakeShopDAO {
+    /*
+    Methods:
+    
+    getUser(String username)
+    getUsers()
+    getCupcake(String name)
+    getAllCupcakes()
+    -----------
+    addNewUser(User user)
+//    addCupcake(Cupcake cupcake)
+//    addBottom(Bottom bottom)
+//    addTop(Top top)
+    
+    */
+    private final String GET_USER_BY_USERNAME = "SELECT username, password, balance FROM recipesDB.User WHERE username = ?";
 
-    DBConnector db;
-    Connection con = db.getConnection();
-    public CupcakeShopDAO() throws Exception {
-        
+    private DBConnector db;
+    private Connection con;
+    
+    public CupcakeShopDAO() throws Exception {        
         try {
             this.db = new DBConnector();
+            con = db.getConnection();
         } catch (Exception ex) {
             Logger.getLogger(CupcakeShopDAO.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Couldn't connect to DB");
         }
     }
+    
+    /** Returns a User object if a matching username is found.
+     *  Returns null if username gives no match.
+     * @param username
+     * @return 
+     */
+    public User getUser(String username) {
+        if (username == null || username.isEmpty()) {
+            return null;
+        }
+        
+        
+        
+        return null;
+    }
+    
 
-    public void createNewUser(User user) {
+    public void addNewUser(User user) {
+        
         
     }
 }
