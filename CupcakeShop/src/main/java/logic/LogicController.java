6634207e36354f8d9ff8f2b6c4c4bb0c4be74f02
Jonatan Hjelm <cph-jh409@cphbusiness.dk>
2018-09-28 14:02:34 +0200

    qty

--- CupcakeShop/src/main/java/logic/LogicController.java
+++ CupcakeShop/src/main/java/logic/LogicController.java
@@ -30,7 +30,7 @@ public class LogicController {
                 menuHTML = "<a id=\"login\" href=\"?origin=logout\">logout</a>"
                         + " <a id=\"cartIcon\" href =\"?origin=shoppingbasket\"><i style =\"font-size:21px\" class=\"fa\">&#xf07a;</i></a>"
                         + "<h4 id=\"user\" > Logged in as: " + user.getUsername()
-                        + " Balance: " + user.getBalance() + " kr</h4>\n"  ;
+                        + " Balance: " + user.getBalance() + " kr</h4>\n";
                 return menuHTML;
             } catch (NullPointerException ne) {
                 ne.printStackTrace();
@@ -57,20 +57,32 @@ public class LogicController {
             toppingsDropdown += "<option value='" + toppings.get(i).getName() + "'>" + toppings.get(i).getName() + ", " + toppings.get(i).getPrice() + " kr</option>";
         }
         toppingsDropdown += "</select>";
+        toppingsDropdown += "<select name='qty' form='extras'>"
+                + "<option value='1'>1</option>"
+                + "<option value='2'>2</option>"
+                + "<option value='3'>3</option>"
+                + "<option value='4'>4</option>"
+                + "<option value='5'>5</option>"
+                + "<option value='6'>6</option>"
+                + "<option value='7'>7</option>"
+                + "<option value='8'>8</option>"
+                + "<option value='9'>9</option>"
+                + "</select>";
+
         return toppingsDropdown;
     }
-    
-    public String generateShoppingCart(ShoppingBasket sb){
+
+    public String generateShoppingCart(ShoppingBasket sb) {
         ArrayList<LineItem> products = sb.getBasket();
         int totalPrice = 0;
-        String cartTable = "<table id=\"shoppingCart\">" + 
-                "<tr><th>Product</th><th>Quantity</th><th>Price</th></tr>";
+        String cartTable = "<table id=\"shoppingCart\">"
+                + "<tr><th>Product</th><th>Quantity</th><th>Price</th></tr>";
         for (int i = 0; i < products.size(); i++) {
-            cartTable += "<tr><td>"+products.get(i).getCake()+"</td>"
-                    + "<td>"+products.get(i).getQuantity()+"</td>"
-                    + "<td>"+products.get(i).getCake().getFullPrice()+" kr</td></tr>";
+            cartTable += "<tr><td>" + products.get(i).getCake() + "</td>"
+                    + "<td>" + products.get(i).getQuantity() + "</td>"
+                    + "<td>" + products.get(i).getCake().getFullPrice() + " kr</td></tr>";
         }
-        cartTable += "<tr id=\"totalPrice\" ><td>Total Price</td><td></td><td>"+sb.getTotalPrice()+" kr</td></tr></table>";
+        cartTable += "<tr id=\"totalPrice\" ><td>Total Price</td><td></td><td>" + sb.getTotalPrice() + " kr</td></tr></table>";
         return cartTable;
     }
 
